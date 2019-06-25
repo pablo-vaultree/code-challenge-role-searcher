279a81c4bc30481d7659952a79fd6997b79824a5
Pablo Feijó <ruanmyv@gmail.com>
2019-06-25 20:14:03 -0300

    starting project

---
+++ role-searcher.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace role_searcher.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ role-searcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace role_searcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
